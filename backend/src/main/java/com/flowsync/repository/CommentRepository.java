52d05b57bd8e39c10303653807d1423fd42097bd
kumar-arav <aravind.sathishKumar@sorimtechnologies.com>
2026-07-31 16:42:50 +0530

    Fix notification and comment repository type mismatches to resolve compiler errors

--- backend/src/main/java/com/flowsync/repository/CommentRepository.java
+++ backend/src/main/java/com/flowsync/repository/CommentRepository.java
@@ -2,7 +2,7 @@ package com.flowsync.repository;
 import com.flowsync.entity.Comment;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
-public interface CommentRepository extends MongoRepository<Comment, String> {
-    List<Comment> findByTicket_IdOrderByCreatedAtAsc(String ticketId);
-    void deleteByAuthor_Id(String authorId);
+public interface CommentRepository extends MongoRepository<Comment, Long> {
+    List<Comment> findByTicket_IdOrderByCreatedAtAsc(Long ticketId);
+    void deleteByAuthor_Id(Long authorId);
 }
--- backend/src/main/java/com/flowsync/repository/NotificationRepository.java
+++ backend/src/main/java/com/flowsync/repository/NotificationRepository.java
@@ -2,9 +2,9 @@ package com.flowsync.repository;
 import com.flowsync.entity.Notification;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
-public interface NotificationRepository extends MongoRepository<Notification, String> {
-    List<Notification> findByRecipient_IdOrderByCreatedAtDesc(String userId);
-    List<Notification> findByRecipient_IdAndReadFalse(String userId);
-    long countByRecipient_IdAndReadFalse(String userId);
-    void deleteByRecipient_Id(String userId);
+public interface NotificationRepository extends MongoRepository<Notification, Long> {
+    List<Notification> findByRecipient_IdOrderByCreatedAtDesc(Long userId);
+    List<Notification> findByRecipient_IdAndReadFalse(Long userId);
+    long countByRecipient_IdAndReadFalse(Long userId);
+    void deleteByRecipient_Id(Long userId);
 }
